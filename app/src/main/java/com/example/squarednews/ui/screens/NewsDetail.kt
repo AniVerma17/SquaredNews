005a2394987bd7fe4c987a95f4946911fea126dc
Animesh Verma <animesh.verma@squareboat.com>
2022-09-06 17:16:56 +0530

    CHANGE: Text alignment of description in news detail screen

--- app/src/main/java/com/example/squarednews/ui/screens/NewsDetail.kt
+++ app/src/main/java/com/example/squarednews/ui/screens/NewsDetail.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -108,7 +109,8 @@ fun newsDetailView(article: Article, onShowStoryClicked: (Article) -> Unit, onBa
                 modifier = Modifier
                     .weight(1f, fill = false)
                     .padding(horizontal = 16.dp),
-                overflow = TextOverflow.Ellipsis
+                overflow = TextOverflow.Ellipsis,
+                textAlign = TextAlign.Justify
             )
             Spacer(modifier = Modifier.height(24.dp))
             TextButton(
